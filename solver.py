9153cc96b5174fe0aec7c7d40f7a64d40595b131
pacimatt-debug <paci.matt@ittmarconiforli.edu.it>
2025-12-12 08:46:44 +0100

    solver.py

--- solver.py
+++ solver.py
@@ -1,131 +1,157 @@
 import numpy as np
+import pandas as pd
 from scipy.optimize import fsolve
 
 class SuspensionSolver:
     def __init__(self, geom, stack_data):
-        # --- 1. GEOMETRIA BASE ---
-        self.type = geom.get('type', 'Fork')
+        # --- 1. GEOMETRIA & UNITÀ (SI: Metri, Pascal, Newton) ---
         self.d_valve = float(geom.get('d_valve', 34.0)) / 1000.0
         self.d_rod = float(geom.get('d_rod', 12.0)) / 1000.0
         self.d_clamp = float(geom.get('d_clamp', 12.0)) / 1000.0
         
-        # --- 2. PORTE, DECK E FLUSSO ---
+        # Geometria Porte
         self.n_port = int(geom.get('n_port', 4))
         self.d_throat = float(geom.get('d_throat', 10.0)) / 1000.0
-        self.n_throat = int(geom.get('n_throat', 4)) 
-        self.r_port = float(geom.get('r_port', self.d_valve*0.35)) / 1000.0
-        self.w_port = float(geom.get('w_port', self.d_valve*0.4)) / 1000.0
-        
-        # CORREZIONE: h.deck è geometria fissa (altezza ingresso porta)
-        self.h_deck = float(geom.get('h_deck', 2.0)) / 1000.0 
+        self.n_throat = int(geom.get('n_throat', 4))
+        self.r_port = float(geom.get('r_port', 12.0)) / 1000.0 # Raggio applicazione forza
+        self.w_port = float(geom.get('w_port', 14.0)) / 1000.0 # Larghezza arco
+        self.h_deck = float(geom.get('h_deck', 2.0)) / 1000.0  # Altezza ingresso flusso
         
         self.bleed = float(geom.get('bleed', 1.5)) / 1000.0
         self.d_leak = float(geom.get('d_leak', 0.0)) / 1000.0
-
-        # --- 3. PRESSURIZZAZIONE ---
-        self.p_zero = float(geom.get('p_zero', 1.5)) * 100000 
-        self.k_ics = float(geom.get('k_ics', 0.0)) * 9800 
-        self.flt_ics = float(geom.get('flt_ics', 0.0)) / 1000.0
-        self.l_ics = float(geom.get('l_ics', 0.0)) / 1000.0
-        self.d_ics = float(geom.get('d_ics', 24.0)) / 1000.0
-        self.id_ics = float(geom.get('id_ics', 10.0)) / 1000.0
-        self.rho = float(geom.get('oil_density', 870.0))
         
-        # --- 4. STACK & FLOAT (GIOCO) ---
-        if isinstance(stack_data, list):
-            self.shims = stack_data
-            self.stack_float = 0.0
-        else:
-            self.shims = stack_data.get('shims', [])
-            # CORREZIONE: Questo è il gioco del pacco (ex h.deck nel simulatore)
-            self.stack_float = float(stack_data.get('stack_float', 0.0)) / 1000.0
+        # Fisica Fluido
+        self.p_zero = float(geom.get('p_zero', 1.5)) * 100000 # Bar -> Pascal
+        self.rho = float(geom.get('oil_density', 870.0)) # kg/m^3
         
-        # Aree
+        # --- 2. STACK LAMELLE ---
+        self.shims = stack_data.get('shims', [])
+        self.stack_float = float(stack_data.get('stack_float', 0.0)) / 1000.0
+        
+        # Calcolo Rigidezza (K_stack) usando Roark's Formulas
+        self.k_stack_equiv = self._calculate_stiffness()
+        
+        # Calcolo Aree Idrauliche (Mid Valve = Annulus)
         self.A_rod = np.pi * (self.d_rod/2)**2
         self.A_valve = np.pi * (self.d_valve/2)**2
-        self.A_active = self.A_valve - self.A_rod
-        
-        self.k_shims = self._calculate_shim_stiffness()
+        self.A_piston = self.A_valve - self.A_rod 
 
-    def _calculate_shim_stiffness(self):
+    def _calculate_stiffness(self):
         k_tot = 0.0
-        lever = self.r_port - (self.d_clamp / 2.0)
-        if lever <= 0: lever = 0.001
+        lever_arm = self.r_port - (self.d_clamp / 2.0)
+        if lever_arm <= 0: lever_arm = 0.001
         
-        for shim in self.shims:
-            od = float(shim['od']) / 1000.0
-            th = float(shim['th']) / 1000.0
-            if (od/2) <= (self.d_clamp/2): continue
+        for s in self.shims:
+            od = float(s['od']) / 1000.0
+            th = float(s['th']) / 1000.0
+            if od/2 <= self.d_clamp/2: continue 
+            
             r_ratio = (od - self.d_clamp) / 2
             if r_ratio <= 0: continue
-            k_s = (210e9 * th**3) / (lever * r_ratio) * 0.15 
-            k_tot += k_s
+            
+            # Modello analitico piastra circolare (FEM approx)
+            # E = 210 GPa (Acciaio armonico)
+            k_shim = (210e9 * th**3 * 0.15) / (lever_arm * r_ratio**1.5) 
+            k_tot += k_shim
         return k_tot
 
-    def calculate_back_pressure(self):
-        if self.type == "Shock": return self.p_zero 
-        elif self.type == "Fork":
-            if self.l_ics > 0 and self.d_ics > 0:
-                f_spring = self.k_ics * self.flt_ics
-                a_ics = np.pi*((self.d_ics/2)**2 - (self.id_ics/2)**2)
-                return self.p_zero + (f_spring / a_ics if a_ics > 0 else 0)
-            else: return self.p_zero
-        return 0.0
-
-    def calculate_force(self, v, clicker_pct=100):
-        if v == 0: return 0
-        Q_target = v * self.A_active
-        Cd = 0.7
+    def solve_point(self, v_shaft, clicker_pct=100):
+        """
+        Risolve l'equilibrio idraulico usando Bernoulli.
+        Trova la Pressione (dP) tale che: Flusso_Calcolato(dP) == Flusso_Target(v_shaft)
+        """
+        if v_shaft == 0: 
+            return {"v":0, "force":0, "lift":0, "pressure_bar":0, "area_eff_mm2":0}
         
-        area_clicker = np.pi * ((self.bleed * clicker_pct/100)/2)**2
-        area_leak = np.pi * (self.d_leak/2)**2
-        area_bypass = area_clicker + area_leak
+        # Flusso Target Q [m^3/s] = Velocità * Area Pistone
+        Q_target = v_shaft * self.A_piston
+        Cd = 0.7 # Coefficiente di scarico (forma orifizio)
         
-        p_back = self.calculate_back_pressure()
-        f_rod_gas = p_back * self.A_rod
-
-        # CALCOLO AREE FISSE DI RESTRIZIONE
-        # 1. Throat Area (Gola interna)
+        # --- AREE GEOMETRICHE FISSE ---
+        # 1. Bypass (Clicker + Leak)
+        A_bleed = np.pi * ((self.bleed * clicker_pct/100)/2)**2 + np.pi*(self.d_leak/2)**2
+        
+        # 2. Throat (Gola interna del pistone)
         A_throat = self.n_throat * np.pi * (self.d_throat/2)**2
         
-        # 2. Deck Area (Ingresso Porta - Parametro h.deck)
-        # Area rettangolare equivalente ingresso
+        # 3. Deck (Ingresso porta rettangolare)
         A_deck = self.n_port * (self.w_port * self.h_deck)
 
-        def pressure_eq(dp):
+        # --- EQUAZIONE DI BERNOULLI DA RISOLVERE ---
+        def flow_equilibrium(dp):
             if dp <= 0: return -Q_target
             
-            # Flusso Bypass
-            q_bypass = Cd * area_bypass * np.sqrt(2 * dp / self.rho)
+            # A. Flusso attraverso Bleed (Sempre aperto)
+            # Bernoulli: Q = Cd * A * sqrt(2 * dP / rho)
+            Q_bleed = Cd * A_bleed * np.sqrt(2 * dp / self.rho)
             
-            # Flusso Main
-            # Forza idraulica
+            # B. Flusso attraverso Main Stack (Variabile)
+            # 1. Calcolo Forza Idraulica che spinge le lamelle
+            #    F = Pressione * Area_Porta_Esposta
             area_force = self.n_port * (self.w_port * (self.r_port - self.d_clamp/2))
-            f_hyd = dp * area_force
+            F_hyd = dp * area_force
             
-            # Lift = Float (Gioco) + Flessione
-            lift = self.stack_float # Inizia dal gioco libero
+            # 2. Calcolo Lift (Apertura) in base alla forza
+            lift = self.stack_float # Inizia dal gioco libero (Float)
+            if self.k_stack_equiv > 100: 
+                lift += F_hyd / self.k_stack_equiv
+            elif self.stack_float <= 0:
+                lift += 0.005 # Minimo spiraglio fisico
             
-            if self.k_shims > 1:
-                lift += f_hyd / self.k_shims
-            elif self.k_shims <= 1 and self.stack_float <= 0:
-                lift += 0.005 # Minima apertura
+            # 3. Calcolo Area Tenda (Curtain Area) generata dal Lift
+            perimeter = self.n_port * (2*self.w_port + 2*(self.d_valve/2 - self.r_port))
+            A_curtain = perimeter * lift
             
-            # 3. Curtain Area (Tenda variabile)
-            perim = self.n_port * (2*self.w_port + 2*(self.d_valve/2 - self.r_port))
-            A_curtain = perim * lift
+            # 4. RESTRIZIONI IN SERIE (Il collo di bottiglia)
+            # L'olio deve passare da Deck -> Throat -> Curtain.
+            # L'area efficace è dominata dalla più piccola.
+            # Formula idraulica: 1/A_eff^2 = 1/A1^2 + 1/A2^2 ...
             
-            # IL FLUSSO È LIMITATO DALL'AREA PIÙ PICCOLA
-            # Se h.deck è piccolo, strozza il flusso anche se le lamelle aprono tanto
-            A_effective = min(A_curtain, A_throat, A_deck)
+            inv_sq_sum = 0
+            if A_deck > 0: inv_sq_sum += 1/(A_deck**2)
+            if A_throat > 0: inv_sq_sum += 1/(A_throat**2)
+            if A_curtain > 0: inv_sq_sum += 1/(A_curtain**2)
             
-            q_valve = Cd * A_effective * np.sqrt(2 * dp / self.rho)
+            if inv_sq_sum > 0:
+                A_effective_main = np.sqrt(1 / inv_sq_sum)
+            else:
+                A_effective_main = 0
             
-            return (q_bypass + q_valve) - Q_target
+            # Bernoulli sul Main Stack
+            Q_main = Cd * A_effective_main * np.sqrt(2 * dp / self.rho)
+            
+            # Residuo (deve essere 0)
+            return (Q_bleed + Q_main) - Q_target
 
+        # Risolutore numerico (Trova la dP corretta)
         try:
-            dp = fsolve(pressure_eq, 10e5)[0]
+            # Start guess: 10 bar (1e6 Pa)
+            dp_sol = fsolve(flow_equilibrium, 1e6)[0] 
         except:
-            dp = 0
+            dp_sol = 0
             
-        return (dp * self.A_active + f_rod_gas) / 9.81
+        # --- CALCOLO OUTPUT FINALI ---
+        # Forza Smorzante Idraulica = dP * Area Pistone
+        force_damp = dp_sol * self.A_piston
+        
+        # Forza Back Pressure (Gas/ICS sull'asta)
+        force_rod = self.p_zero * self.A_rod
+        
+        # Ricalcolo Lift finale per visualizzazione
+        area_force = self.n_port * (self.w_port * (self.r_port - self.d_clamp/2))
+        lift_final = self.stack_float + (dp_sol * area_force / self.k_stack_equiv if self.k_stack_equiv>0 else 0)
+        
+        # Ricalcolo Area Efficace totale
+        total_flow = Q_target
+        if dp_sol > 0:
+            area_eff_total = total_flow / (Cd * np.sqrt(2*dp_sol/self.rho))
+        else:
+            area_eff_total = 0
+
+        return {
+            "v": v_shaft,
+            "force_kg": (force_damp + force_rod) / 9.81, 
+            "lift_mm": lift_final * 1000,
+            "pressure_bar": dp_sol / 100000,
+            "area_eff_mm2": area_eff_total * 1e6
+        }
